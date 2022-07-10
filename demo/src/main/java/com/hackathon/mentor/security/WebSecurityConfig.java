91848410fa0c0ca4f7e651a22aacf45eb0bdfacd
dias.bagzat <diasbagzat1@gmail.com>
2022-07-10 21:38:29 +0600

    v1_commit

--- demo/src/main/java/com/hackathon/mentor/security/WebSecurityConfig.java
+++ demo/src/main/java/com/hackathon/mentor/security/WebSecurityConfig.java
@@ -54,7 +54,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable()
+		http
+				.httpBasic().and()
+				.cors().and().csrf().disable()
 			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
